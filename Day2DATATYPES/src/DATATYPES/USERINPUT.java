2f907a18b623b0160c941b03b9e83243507aad77
HP <HP@DESKTOP-FCN0OTR>
2022-11-27 19:38:06 +0530

    first commit

---
+++ Day2DATATYPES/src/DATATYPES/USERINPUT.java
@@ -0,0 +1,26 @@
+package DATATYPES;
+import java.util.Scanner;
+
+
+public class USERINPUT {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("Enter the number: ");
+		int x=s.nextInt();
+		
+		System.out.println("Enter the number: ");
+		float y=s.nextFloat();
+		
+		System.out.println("Enter the char: ");
+		char c=s.next().charAt(0);		
+		System.out.println("The integer is: "+x);
+		System.out.println("The decimal is: "+y);
+		System.out.println("The character is: "+c);
+		s.close();
+
+	}
+
+}
+
