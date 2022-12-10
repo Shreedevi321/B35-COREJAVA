a599b0333a1f0813ce09d58940541780a3f07632
HP <HP@DESKTOP-FCN0OTR>
2022-12-10 21:31:49 +0530

    first commit

---
+++ Day-13_ExceptionHandling/src/customexceptiondemo/Executor.java
@@ -0,0 +1,23 @@
+package customexceptiondemo;
+
+import java.util.Scanner;
+//driver class for custom exception
+public class Executor {
+
+	public static void main(String[] args) throws LoginCredentials  {
+		Scanner s=new Scanner(System.in);
+		String id=s.nextLine();
+		String password=s.nextLine();
+		//validating
+		if(id!="shreedeviitagi58@gmil com" && password!="123abc")
+		{
+			throw new LoginCredentials(id, password);
+		}
+		
+			
+
+		
+
+	}
+
+}
---
+++ Day-13_ExceptionHandling/src/exceptiondemo/ExecutorForThrow.java
@@ -0,0 +1,12 @@
+package exceptiondemo;
+
+//driver class
+public class ExecutorForThrow {
+
+	public static void main(String[] args) {
+		DemoOnThrowKeyword d=new DemoOnThrowKeyword();
+		d.display("Welcome to TNS India");
+
+	}
+
+}
