74f5f7cc7b2ef7589bf0549cd2439a80c91bf092
HP <HP@DESKTOP-FCN0OTR>
2022-12-24 18:13:00 +0530

    first commit

---
+++ Day20_JDBC_CRUD/src/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package service;
+
+
+import java.util.List;
+
+import entities.Employee;
+
+public interface EmployeeService {
+	
+	//by default all the methods are abstract in an interface
+	
+	//CRUD user defined method
+	public boolean AddEmployee(Employee emp);
+	public Employee UpdateEmployee(Employee emp);
+	public boolean DeleteEmployee(int ID);
+	public Employee GetEmployee(int ID);
+	public List<Employee> GetAll();
+
+}
